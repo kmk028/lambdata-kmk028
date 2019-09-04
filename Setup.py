bb4843e1dfa00aa458a2639b20a5a079d6198e88
kmk028 <kmk028@users.noreply.github.com>
2019-09-03 20:45:10 -0700

    Added twine and __init.py and Setup.py

---
+++ Setup.py
@@ -0,0 +1,31 @@
+"""
+lambdata-kmk028- A Collection of Data Science helper functions.
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata-kmk028",
+    version="0.0.1",
+    author="karthik-mahendra",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kmk028/lambdata-kmk028",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_required=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+lambdata-kmk028
+"""
+
+import pandas as pd
+import numpy as np
+
+ZEROS = np.zeros(5)
+ONES = np.ones(10)
+
