575b32addbec6654692607cd7601d4016ffc68ef
kmk028 <kmk028@users.noreply.github.com>
2019-09-08 18:07:10 -0700

    made code pep8 compliant

--- lambdata_karthik/__init__.py
+++ lambdata_karthik/__init__.py
@@ -10,27 +10,35 @@ import seaborn as sns
 ZEROS = np.zeros(5)
 ONES = np.ones(10)
 
-#Takes Dataframe as input and returns NaNs for each column. Output: Pandas Series
-def df_nulls(df):
-    print(f'Number of null values in each column is: \n{df.isnull().sum(axis = 0)}.')
+# Takes Dataframe as input and returns NaNs for each column. Output:
+# Pandas Series
 
-#inputs true and corresponding Predicted values and plots confusion matrix 
-def plot_confusion_matrix(y_true,y_pred,normalize=False):
+
+def df_nulls(df):
+    print(
+        f'Number of null values in each column is: \n
+        {df.isnull().sum(axis=0)}.')
+
+# inputs true and corresponding Predicted values and plots confusion matrix
+
+
+def plot_confusion_matrix(y_true, y_pred, normalize=False):
     columns = [f'Predicted "{c}"'for c in unique_labels(y_pred)]
     index_names = [f'Actual "{c}"'for c in unique_labels(y_true)]
-    cm = confusion_matrix(y_true,y_pred)
+    cm = confusion_matrix(y_true, y_pred)
     if normalize:
-        cm=cm/cm.sum(axis=1).reshape(y_true.nunique(),1)
-    df = pd.DataFrame(cm,columns = columns,index = index_names)
-    sns.heatmap(df,cmap='viridis',annot=True,fmt='.2f');
+        cm = cm / cm.sum(axis=1).reshape(y_true.nunique(), 1)
+    df = pd.DataFrame(cm, columns=columns, index=index_names)
+    sns.heatmap(df, cmap='viridis', annot=True, fmt='.2f')
 
-def datetime_split(df,col):
-    df[f'{col}'] = pd.to_datetime(df[f{'col'}],infer_datetime_format=True)
+# Splits a datetime column into its day, month and year columns
+# returns the dataframe after dropping original datetime column
+
+
+def datetime_split(df, col):
+    df[f'{col}'] = pd.to_datetime(df[f{'col'}], infer_datetime_format=True)
     df[f'{col}_year'] = df[f'{col}'].dt.year
     df[f'{col}_month'] = df[f'{col}'].dt.month
     df[f'{col}_day'] = df[f'{col}'].dt.day
-    df = df.drop(columns = f'{col}')
+    df = df.drop(columns=f'{col}')
     return df
-
-
-
